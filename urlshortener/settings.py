74f243d8f861ff19db71c8d3cc1d22e61400865d
Sourav Khoso <souravkhoso1@gmail.com>
2021-05-30 20:21:21 +0000

    added whitenoise and related configs

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -120,14 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR),'static')
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR),'static')
+#PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    BASE_DIR / "static",
+    #os.path.join(PROJECT_ROOT, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
